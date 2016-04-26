c732fd9b7ae3d278eae770243799df50947f2fdb
Jonathan Demers <jdemers@zoommed.com>
2016-04-26 10:52:00 -0400

    I created a new project to help me familiarize with TDD concepts.  The repository is now configured to save NuGet packages correctly.  Added a new test dll called Tdd.Tests using Moq, xUnit and AutoFixture to start.

---
+++ Tdd.Tests/IFoo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TddTests
+{
+    public interface IFoo
+    {
+        bool DoSomething(string str);
+        string DoSomethingStringy(string str);
+        bool TryParse(string str, out string outStr);
+        bool Submit(ref Bar inst);
+        int GetCount();
+        int GetCountThing();
+    }
+}
---
+++ Tdd.Tests/MoqCheatSheetTests.cs
@@ -0,0 +1,35 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TddTests
+{
+    public class MoqCheatSheetTests
+    {
+        [Fact]
+        public void Moq_CallToValueReturningMethodWithRightParameter_True()
+        {
+            var sut = new Mock<IFoo>();
+            sut.Setup(x => x.DoSomething("ping")).Returns(true);
+            Assert.Equal(true, sut.Object.DoSomething("ping"));
+        }
+        [Fact]
+        public void Moq_CallToValueReturningMethodWithWrongParameter_False()
+        {
+            var sut = new Mock<IFoo>();
+            sut.Setup(x => x.DoSomething("ping")).Returns(true);
+            Assert.Equal(false, sut.Object.DoSomething("pong"));
+        }
+        [Fact]
+        public void Moq_CallToValueReturningMethodWithoutSetup_Exception()
+        {
+            var sut = new Mock<IFoo>();
+            bool test = sut.Object.DoSomething("ping");
+            Assert.Equal(true, sut.Object.DoSomething("ping"));
+        }
+    }
+}
