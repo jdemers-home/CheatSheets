ef10fd3f68defac2fa22c6fd340952caad2151b7
Jonathan Demers <jdemers@zoommed.com>
2016-05-16 16:23:45 -0400

    AutoFixture cheat sheet added.

--- Tdd.Tests/Bar.cs
+++ Tdd.Tests/Bar.cs
@@ -2,5 +2,17 @@
 {
     public class Bar
     {
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public byte Age { get; set; }
+
+        public Bar()
+        {
+            FirstName = "Jane";
+            MiddleName = "";
+            LastName = "Smith";
+            Age = 25;
+        }
     }
 }
